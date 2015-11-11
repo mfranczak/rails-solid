65c6f475af3c9a49df8f7cbe259425ba5a8d1459
mfranczakhelpling <michal.franczak@helpling.com>
2015-11-11 14:46:55 +0100

    Comment added to the AppointmentsNumberRule

--- app/models/appointments_number_rule.rb
+++ app/models/appointments_number_rule.rb
@@ -3,6 +3,7 @@ class AppointmentsNumberRule
     @max_number = max_number
   end
 
+  # This rule applies when the number of bookings on the same day as new booking is >= to max_number
   def applies?(booking)
     appointment_day_count = Booking.where(clown: booking.clown, appointment_date: booking.appointment_date).count
     appointment_day_count >= @max_number
