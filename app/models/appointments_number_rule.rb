9643b829ba26de7f46dd3ecc66b7cd86066fad67
mfranczakhelpling <michal.franczak@helpling.com>
2015-11-11 14:46:15 +0100

    Code prepared to apply OCP

--- app/models/appointments_number_rule.rb
+++ app/models/appointments_number_rule.rb
@@ -1,10 +1,8 @@
 class AppointmentsNumberRule
   def initialize(max_number)
-    @max_number = max_number
   end
 
+  # This rule applies when the number of bookings on the same day as new booking is >= to max_number
   def applies?(booking)
-    appointment_day_count = Booking.where(clown: booking.clown, appointment_date: booking.appointment_date).count
-    appointment_day_count >= @max_number
   end
 end
--- app/models/booking_clown_appointments_validator.rb
+++ app/models/booking_clown_appointments_validator.rb
@@ -1,7 +1,23 @@
 class BookingClownAppointmentsValidator < ActiveModel::Validator
   def validate(booking)
-    if AppointmentsRuleFactory.create(booking).applies?(booking)
+    appointment_day = Booking.where(clown: booking.clown, appointment_date: booking.appointment_date).count
+
+    max_per_day = 0
+
+    # [OCP]: This switch is a smell for Open/Closed principle violation.
+    # Adding new rule should not require changing the validator
+    case booking.clown.contract.to_sym
+      when :fulltime
+        max_per_day = 3
+      when :parttime
+        max_per_day = 2
+      when :student
+        max_per_day = 1
+    end
+
+    if appointment_day >= max_per_day
       booking.errors.add :appointment_date, 'This clown can not have more appointments. Try to change the date.'
     end
+
   end
 end
