8fc7b6f34c1828373128449983f65d20b85b46ce
mfranczakhelpling <michal.franczak@helpling.com>
2015-11-11 14:24:05 +0100

    Code prepared to apply SRP

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -16,6 +16,31 @@ class Booking < ActiveRecord::Base
 
   private
 
+  # Depending on the clowns contract they are not allowed to take more appointments than X a day.
+  #
+  # [SRP]: This business logic should not be placed in the Model class. Extract it to a validator class
+  # http://guides.rubyonrails.org/active_record_validations.html#custom-validators
+  def number_of_appointments_per_day
+    appointment_day = Booking.where(clown: clown, appointment_date: appointment_date).count
+
+    max_per_day = 0
+
+    # [OCP]: This switch is a smell for Open/Closed principle violation.
+    # Adding new rule should not require changing the validator
+    case clown.contract.to_sym
+      when :fulltime
+        max_per_day = 3
+      when :parttime
+        max_per_day = 2
+      when :student
+        max_per_day = 1
+    end
+
+    if appointment_day >= max_per_day
+      errors.add :appointment_date, 'This clown can not have more appointments. Try to change the date.'
+    end
+  end
+
   # [SRP*] Can be moved to a separate validator class.
   def already_has_an_appointment
     if clown.has_appointment? appointment_date, appointment_time
